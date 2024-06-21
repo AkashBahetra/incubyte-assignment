407fa3a398eb38e81640dc0f25392bf709fc3eb1
AkashBahetra <akashbahetra05@gmail.com>
2024-06-21 13:27:35 +0530

    Update method to allow ustom delimiter

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -6,11 +6,6 @@ describe('add Numbers', () => {
   })
 
   it("should return added sum of numbers from the input string", () => {
-    const actual = add("148");
-    expect(actual).toBe(13);
-  })
-
-  it("should take , as a delimiter", () => {
     const actual = add("1,4,8");
     expect(actual).toBe(13);
   })
