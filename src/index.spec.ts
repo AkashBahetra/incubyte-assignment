5d7b30e51b5da0730d3d03c161528a9ba100d18f
AkashBahetra <akashbahetra05@gmail.com>
2024-06-21 12:54:01 +0530

    Added test to check if it returns the sum

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,4 +4,9 @@ describe('add Numbers', () => {
   it('exists', () => {
     expect(add).toBeDefined();
   })
+
+  it("should return added sum of numbers from the input string", () => {
+    const actual = add("148");
+    expect(actual).toBe(13);
+  })
 });
